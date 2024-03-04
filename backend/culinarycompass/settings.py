bf52e887dede1db4773c691ea4e66b851673402b
Roehit Ranganathan <roehitr1@gmail.com>
2024-03-04 13:50:47 -0500

    DB_Config

--- backend/culinarycompass/settings.py
+++ backend/culinarycompass/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'culinarycompass.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
