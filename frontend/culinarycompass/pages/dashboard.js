0e415404dd735a7603c96cdf0f57a5ca97bce451
NITHIN_AAKASH <snithinaakash@gmail.com>
2024-04-06 11:24:40 -0400

    Dashboard filled with Images

--- frontend/culinarycompass/pages/dashboard.js
+++ frontend/culinarycompass/pages/dashboard.js
@@ -58,9 +58,89 @@ const Dashboard = () => {
                     <button className="nav-button" onClick={handleLogout}>Logout</button>
                 </div>
             </nav>
-            <div className="center">
+            <div className="top">
                 <h1 className="title">Welcome to Culinary Compass</h1>
             </div>
+            {/* <div>
+            <img src="images/Food_Logo.jpg"  width={500}  height={300}/>         
+   <a href="https://www.theculinarycompass.com/grilled-boneless-skinless-chicken-thighs/" target="_self">Grilled Boneless Skinless Chicken Thighs</a>
+            </div> */}
+            <div className="imageContainer">
+      <div className="imageItem">
+        <img
+          src="images/Chicken_Manchurian.jpg"
+          alt="Image 1"
+          width={400}
+          height={300}
+          layout="intrinsic"
+        /> 
+        <br></br>
+        <a href="https://www.theculinarycompass.com/grilled-boneless-skinless-chicken-thighs/" target="_self">Grilled Boneless Skinless Chicken Thighs</a>
+          
+      </div>
+      <div className="imageItem">
+        <img
+          src="images/salmon.jpg"
+          alt="Image 2"
+          width={400}
+          height={300}
+          layout="intrinsic"
+        />
+        <br></br>
+                <a href="https://www.theculinarycompass.com/grilled-boneless-skinless-chicken-thighs/" target="_self">Salmon Fry</a>
+          
+      </div>
+      <div className="imageItem">
+        <img
+          src="images/Chorizo-Cheese-Dip.jpg"
+          alt="Image 3"
+          width={400}
+          height={300}
+          layout="intrinsic"
+        />
+                <br></br>
+                <a href="https://www.theculinarycompass.com/grilled-boneless-skinless-chicken-thighs/" target="_self">Choriza-Cheese-Dip</a>
+          
+      </div>
+    </div>
+    <div className="imageContainer">
+      <div className="imageItem">
+        <img
+          src="images/Firecracker-Meatballs.jpg"
+          alt="Image 1"
+          width={400}
+          height={300}
+          layout="intrinsic"
+        />
+                  <br></br>
+                <a href="https://www.theculinarycompass.com/grilled-boneless-skinless-chicken-thighs/" target="_self">Firecracker-Meatballs</a>
+          
+      </div>
+      <div className="imageItem">
+        <img
+          src="images/Old-Bay-Steamed-Shrimp.jpg"
+          alt="Image 2"
+          width={400}
+          height={300}
+          layout="intrinsic"
+        />
+            <br></br>
+                <a href="https://www.theculinarycompass.com/grilled-boneless-skinless-chicken-thighs/" target="_self">Old-Bay-Steamed-Shrimp</a>
+          
+      </div>
+      <div className="imageItem">
+        <img
+          src="images/Pull-Apart-Garlic-Bread.jpg"
+          alt="Image 3"
+          width={400}
+          height={300}
+          layout="intrinsic"
+        />
+            <br></br>
+                <a href="https://www.theculinarycompass.com/grilled-boneless-skinless-chicken-thighs/" target="_self">Pull-Apart-Garlic-Bread</a>
+          
+      </div>
+    </div>
         </div>
     );
 };
