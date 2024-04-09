c7bf70e9a153f2c31ea090ca8a17461e8ca3b46b
Abhinav S <abhinavstar.anand@gmail.com>
2024-04-09 19:50:08 -0400

    loader

--- frontend/culinarycompass/pages/dashboard.js
+++ frontend/culinarycompass/pages/dashboard.js
@@ -4,9 +4,11 @@ import { useAuth } from '@/context/Authcontext';
 import './dashboard.css'; 
 const Dashboard = () => {
     const router = useRouter();
-    const [items, setItems] = useState([]);      
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);      
     const { logout, accessToken, refreshToken, getUser, user } = useAuth();
-
+    console.log('User:')
+    console.log(user);
     useEffect(() => {
       if (!accessToken && !refreshToken) {
         router.push('/');
@@ -14,10 +16,12 @@ const Dashboard = () => {
     });
     useEffect(() => {
       getUser();
-      // Simulating fetching JSON data from an API
-      fetch('https://yourapi.com/items')
+      fetch('https://localhost/items')
       .then(response => response.json())
-      .then(data => setItems(data.slice(0, 9))) // Taking only the first 9 items
+      .then(data => {
+        setItems(data.slice(0, 9)); // Taking only the first 9 items
+        setIsLoading(false); 
+      })      
       .catch(error => console.error('Error fetching items:', error));
 
     }, []);
@@ -39,6 +43,7 @@ const Dashboard = () => {
             <nav className="navbar">
             <div className="navbar-content">
             <img src="images/Food_Logo.jpg" alt="Logo" className="logo" />
+            {user && <span>{user.username}</span>}
             </div>
             <div>
             <button className="nav-button" onClick={handleSearch}>Search for Recipe</button>
@@ -47,13 +52,17 @@ const Dashboard = () => {
             </nav>
             <h1 className="title">Welcome to Culinary Compass</h1>
             <div className="grid-container">
-                {items.length > 0 ? items.map((item, index) => (
-                    <div key={index} className="grid-item"> 
-                        {item.image ? <img src={item.image} alt={item.title} /> : null}
-                        <p>{item.title}</p>
-                        <button onClick={() => handleRecipe(item.title)}>View Recipe</button>
-                    </div>
-                )) : <p>No items to display</p>}
+              {isLoading ? (
+                  <div className="loader-container"><div className="loader"></div></div>
+              ) : (
+                  items.length > 0 ? items.map((item, index) => (
+                      <div key={index} className="grid-item"> 
+                          {item.image ? <img src={item.image} alt={item.title} /> : null}
+                          <p>{item.title}</p>
+                          <button onClick={() => handlerecipe(item.title)}>View Recipe</button>
+                      </div>
+                  )) : <p>No items to display</p>
+              )}
             </div>
             <div>
                 <button className="nav-button" onClick={() => router.push('/search')}>Search for Recipe</button>
