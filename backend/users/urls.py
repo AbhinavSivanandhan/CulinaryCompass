7b24523deb703929b11de1536ff0f3bd915b7e79
km6078 <km6078@nyu.edu>
2024-04-17 19:27:37 -0400

    Added tests for user login and registration

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('register/', views.register),
+    path('register/', views.register, name='register'),
     path('login/', views.UserLoginAPIView.as_view(), name='login'),
     path('profile/', views.UserProfileAPIView.as_view(), name='user_profile'),
 ]
