ab5b3a6e63e4da56f8ac939482761022e19eedb1
Roehit Ranganathan <roehitr1@gmail.com>
2024-04-21 17:55:12 -0400

    S3 and static data schema

--- backend/culinarycompass/settings.py
+++ backend/culinarycompass/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'recipes',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# All of this is in my console.aws.amazon to configure aws s3 static files only
+# IAM Management Console
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') 
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
+# Amazon S3 Buckets
+AWS_STORAGE_BUCKET_NAME =  os.environ.get('AWS_STORAGE_BUCKET_NAME') 
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# For some reason I needed static root to collectstatistics
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 # User search history model
 class UserSearchHistory(models.Model):
@@ -10,3 +10,18 @@ class UserSearchHistory(models.Model):
 
     def __str__(self):
         return self.user_id
+    
+# Sample Recipe model
+class SampleRecipe(models.Model):
+    snum = models.IntegerField()
+    name = models.CharField(max_length=1000)
+    recipe_id = models.CharField(max_length=100)
+    minutes = models.IntegerField()
+    tags = models.CharField(max_length=10000)
+    nutrition = ArrayField(models.FloatField(default=0))
+    n_steps = models.IntegerField()
+    steps = ArrayField(models.CharField(max_length=10000))
+    description = models.CharField(max_length=10000)
+    ingredients = ArrayField(models.CharField(max_length=10000))
+    n_ingredients = models.IntegerField()
+    image_url = models.CharField(max_length=1000)
